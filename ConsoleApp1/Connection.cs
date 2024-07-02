5330c44255504ed11b7135602237092fbfb980fe
INFRA\Joevian.CJ <jovicj@indomaret.co.id>
2024-07-02 15:06:15 +0700

    Cek

---
+++ ConsoleApp1/Connection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace ConsoleApp1
+{
+    internal class Connection
+    {
+        public static MySqlConnection ConnClient;
+        public static string ConnStrClient = "server=172.24.16.136;port=3306;" + "user id=prog;password=ind0M4rcO^Pri$;database=test_jovi;" + "Persist Security Info=True;pooling=false;connection timeout=500";
+
+
+        public Connection()
+        {
+            ConnClient = GetKoneksi(ConnStrClient);
+        }
+
+        public static MySqlConnection GetKoneksi(string ConnStr)
+        {
+            MySqlConnection Mcon = new MySqlConnection();
+            try
+            {
+                Mcon = new MySqlConnection(ConnStr);
+                return (MySqlConnection)Mcon.Clone();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message, "KoneksiMySQL");
+                return null/* TODO Change to default(_) if this is not a reference type */;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, "KoneksiMySQL");
+                return null/* TODO Change to default(_) if this is not a reference type */;
+            }
+        }
+        public static MySqlConnection GetClientConn
+        {
+            get
+            {
+                return ConnClient;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                panggil();
+                Console.WriteLine();
+            }
+
+
+        }
+        public static void panggil()
+        {
+            Connection conn = new Connection();
+            string Sql = string.Empty;
+
+            try
+            {
+                if (Connection.GetClientConn.State == ConnectionState.Closed)
+                    Connection.GetClientConn.Open();
+
+                Sql = "Update `" + Connection.GetClientConn.Database + "`.`counter_scale` set counter = NOW();";
+
+                using (MySqlCommand cmd = new MySqlCommand(Sql, Connection.GetClientConn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+            }
+        }
+    }
+}
